297892c6801951ed7c2e7e0a83090db2ad46f0f0
choong <choong83@gmail.com>
2012-04-28 16:02:44 +0900

    test

--- project-a/src/Gugudan.java
+++ project-a/src/Gugudan.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Scanner;
 
 public class Gugudan {
@@ -10,31 +9,25 @@ public class Gugudan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		System.out.println("gugudanfrom:");
 
-		 System.out.println("gugudanfrom:");
+		Scanner sc = new Scanner(System.in);
 
-		 Scanner sc=new Scanner(System.in);
+		int i = sc.nextInt();
 
-		 int i=sc.nextInt();
-		 
-		 	 
-		 for (; i <= 9; i++) 
-		{
-		 	printDan(i);
-	
+		for (; i <= 9; i++) {
+			printDan(i);
+
 		}
 
 	}
 
 	public static void printDan(int i) {
-		
-			for (int j = 1; j <= 9; j++) 
-			{
-				int k =i*j;
-				System.out.println(i + "x" + j + "=" + i*j);
-			}
-		
+
+		for (int j = 1; j <= 9; j++) {
+			System.out.println(i + " x " + j + " = " + i * j);
+		}
+
 	}
 
 }
