7c66e98380801441e6e3f0f5f6e0c4fb71d45866
insight1004 <insight1004@gmail.com>
2012-04-28 16:06:32 +0900

    정렬

--- project-a/src/Gugudan.java
+++ project-a/src/Gugudan.java
@@ -7,17 +7,12 @@ public class Gugudan {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.out.println("gugudanfrom:");
-
 		Scanner sc = new Scanner(System.in);
-
 		int i = sc.nextInt();
-
 		for (; i <= 9; i++) {
 			printDan(i);
-
 		}
 
 	}
