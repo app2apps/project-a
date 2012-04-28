aaf7a0c4c7a92da2677153bad7aa09a2286ac512
insight1004 <insight1004@gmail.com>
2012-04-28 16:40:26 +0900

    test2 kjs

--- project-a/src/Gugudan.java
+++ project-a/src/Gugudan.java
@@ -19,6 +19,7 @@ public class Gugudan {
 	public static void printDan(int i) {
 		//d
 		//test
+		//test2
 		for (int j = 1; j <= 9; j++) {
 			System.out.println(i + " x " + j + " = " + i * j);
 		}
